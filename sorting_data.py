48952c8072c8a430ea1d31d3815fd5c2dd1edbf8
Nisarg Parikh <npnishurox@gmail.com>
2022-11-25 09:49:52 -0500

    Update sorting_data.py

--- sorting_data.py
+++ sorting_data.py
@@ -1,19 +1,25 @@
 import pandas as pd
 from PIL import Image
 
+# Open file with labels
 df = pd.read_csv(r'D:\VA\Python Workspace\SkinCancerDetection\ISIC Dataset\NewDatasets\ISIC-images\metadata_5.csv')
 
+# Isolate relevant feilds
 df = df[['name', 'meta.clinical.benign_malignant']]
 df.set_index('name', inplace=True)
 
+# Load data into a Python Dictionary 
 data = df.to_dict()['meta.clinical.benign_malignant']
 
+# Designate Folder to save data to
 frm = r'ISIC Dataset\NewDatasets\ISIC-images\UDA-1'
 to_b = r'Data\Benign'
 to_m = r'Data\Malignant'
+
 b = 401
 m = 639
 
+# Save data into relevant folders
 for k, v in data.items():
     if v == 'benign':
         img = Image.open(frm + "\\"  + k + ".jpg")
